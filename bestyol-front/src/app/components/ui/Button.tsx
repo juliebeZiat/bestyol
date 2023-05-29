61600b93f756f3d509b9ea25c8a43733c061e209
Alan Houdus <alan.houdus@gmail.com>
2023-05-29 14:28:52 +0200

    ui: add HomePage, add enum size to Button ui component

--- bestyol-front/src/app/components/ui/Button.tsx
+++ bestyol-front/src/app/components/ui/Button.tsx
@@ -1,7 +1,15 @@
 'use client'
+
+export enum ButtonSize {
+	Small = 'py-2 px-4 text-lg',
+	Medium = 'py-3 px-5 text-xl',
+	Large = 'py-5 px-7 text-4xl',
+}
+
 interface ButtonProps {
 	backgroundColor?: string
 	textColor?: string
+	size?: ButtonSize
 	content: string
 	uppercase?: boolean
 	onClick?: () => void
@@ -10,13 +18,14 @@ interface ButtonProps {
 const Button = ({
 	backgroundColor = 'bg-grey',
 	textColor,
+	size = ButtonSize.Medium,
 	content,
 	uppercase,
 	onClick,
 }: ButtonProps) => {
 	return (
 		<button
-			className={`${backgroundColor} ${textColor} py-2 px-4 text-lg`}
+			className={`${backgroundColor} ${textColor} ${size}`}
 			onClick={onClick}
 		>
 			{uppercase ? content.toUpperCase() : content}
