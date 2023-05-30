960c709541a6ab247c8ba31ea7325e20ea73edfd
Alan Houdus <alan.houdus@gmail.com>
2023-05-30 16:11:05 +0200

    ui: Add profile page, profile banner, profile stats, profile informations, profile menu

--- bestyol-front/src/app/(pages)/(connected-user)/profile/page.tsx
+++ bestyol-front/src/app/(pages)/(connected-user)/profile/page.tsx
@@ -1,11 +1,33 @@
 'use client'
 
-import Box from '@/app/components/ui/Box'
+import ProfileBanner from '@/app/components/profile/ProfileBanner'
+import ProfileInformation from '@/app/components/profile/ProfileInformation'
+import ProfileStatistics from '@/app/components/profile/ProfileStatistics'
+import Button from '@/app/components/ui/Button'
+import Modal from '@/app/components/ui/Modal'
+import TextField from '@/app/components/ui/TextField'
+import { useIsMobile } from '@/hooks/useWindowSize'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
+import { availableAvatars, availableBanners } from './profileImages'
 
 const ProfilePage = () => {
+	const [userAvatar, setUserAvatar] = useState<string>(
+		'/assets/avatars/Icon1.png',
+	)
+	const [userBanner, setUserBanner] = useState<string>('/assets/mountain.png')
+	const [modalAvatarIsOpen, setModalAvatarIsOpen] = useState(false)
+	const [modalBannerIsOpen, setModalBannerIsOpen] = useState(false)
+	const [modalInfoIsOpen, setModalInfoIsOpen] = useState(false)
+	const [modalPasswordIsOpen, setModalPasswordIsOpen] = useState(false)
+	const [modalDeleteAccountIsOpen, setModalDeleteAccountIsOpen] =
+		useState(false)
+	const [deleteAccountStateView, setDeleteAccountStateView] = useState<
+		'form' | 'confirmation'
+	>('form')
+
 	const [hydrated, setHydrated] = useState(false)
+	const isMobile = useIsMobile()
 	useEffect(() => {
 		setHydrated(true)
 	}, [])
@@ -15,55 +37,203 @@ const ProfilePage = () => {
 
 	return (
 		<div className='w-full flex flex-col gap-20'>
-			<div className='relative flex items-center h-[200px] bg-lowOpacity'>
-				<Image
-					src='/assets/yol-test-1.png'
-					height={200}
-					width={150}
-					alt='Image de profil'
-					className='ml-10'
+			<ProfileBanner
+				avatar={userAvatar}
+				banner={userBanner}
+				setModalAvatarIsOpen={setModalAvatarIsOpen}
+				setModalBannerIsOpen={setModalBannerIsOpen}
+			/>
+			<div
+				className={
+					'flex justify-center items-center mx-4 text-white gap-5 sm:gap-28 lg:gap-36 ' +
+					(isMobile ? 'flex-col' : '')
+				}
+			>
+				<ProfileInformation
+					setModalInfoIsOpen={setModalInfoIsOpen}
+					setModalPasswordIsOpen={setModalPasswordIsOpen}
+					setModalDeleteAccountIsOpen={setModalDeleteAccountIsOpen}
 				/>
-				<Image
-					src='/assets/mountain.png'
-					height={100}
-					width={300}
-					alt='Fond du profil'
-					className='absolute right-0 bottom-0'
-				/>
+				<ProfileStatistics />
 			</div>
-
-			<div className='flex justify-center items-center mx-4 text-white gap-5 sm:gap-28 lg:gap-36'>
-				<Box additionalStyle='max-w-[300px] min-h-[400px] border-white border-2'>
-					<div className='flex flex-col items-center gap-5'>
-						<h2 className='text-center text-xl sm:text-4xl tracking-wider'>
-							Informations
-						</h2>
-						<div className='flex flex-col gap-5 sm:text-xl justify-between h-[250px]'>
-							<div className='flex flex-col'>
-								<p className=''>nom : Julie</p>
-								<p className=''>email : julie@mail.fr</p>
+			{/* Modal pour modifier l'avatar */}
+			<Modal
+				isOpen={modalAvatarIsOpen}
+				onClose={() => setModalAvatarIsOpen(false)}
+				title='Modifier mon avatar'
+			>
+				<div className='flex flex-col items-center justify-center gap-10 basis-[33%] sm:min-w-[350px] lg:min-w-[500px] min-h-[300px]'>
+					<div className='flex flex-wrap gap-4 justify-center'>
+						{availableAvatars.map((avatar, index) => (
+							<Image
+								key={index}
+								src={avatar}
+								alt='avatar'
+								width={100}
+								height={100}
+								className={`cursor-pointer h-[100px] w-[100px] ${
+									userAvatar === avatar
+										? 'border-2 border-white border-darkLowOpacity'
+										: ''
+								}`}
+								onClick={() => setUserAvatar(avatar)}
+							/>
+						))}
+						{/* random avatar button */}
+						<div
+							className='flex justify-center items-center cursor-pointer h-[100px] w-[100px] border border-darkLowOpacity'
+							onClick={() => {
+								const randomAvatar =
+									availableAvatars[
+										Math.floor(Math.random() * availableAvatars.length)
+									]
+								setUserAvatar(randomAvatar)
+							}}
+						>
+							<div className='text-4xl font-bold text-white select-none'>?</div>
+						</div>
+					</div>
+					<Button
+						content='Valider'
+						onClick={() => setModalAvatarIsOpen(false)}
+						backgroundColor='bg-orange'
+						additionalStyle='self-center w-[50%]'
+					/>
+				</div>
+			</Modal>
+			{/* Modal pour modifier la banner */}
+			<Modal
+				isOpen={modalBannerIsOpen}
+				onClose={() => setModalBannerIsOpen(false)}
+				title='Modifier ma bannière'
+			>
+				<div className='flex flex-col items-center justify-center gap-10 sm:min-w-[350px] lg:min-w-[500px] min-h-[300px]'>
+					<div className='flex flex-wrap gap-4'>
+						{availableBanners.map((banner, index) => (
+							<Image
+								key={index}
+								src={banner}
+								alt='banner'
+								width={200}
+								height={100}
+								className={`cursor-pointer aspect-video object-contain ${
+									userBanner === banner ? 'border-2 border-white' : ''
+								}`}
+								onClick={() => setUserBanner(banner)}
+							/>
+						))}
+						{/* random banner button */}
+						<div
+							className='flex justify-center items-center cursor-pointer h-[100px] w-[100px] border border-darkLowOpacity'
+							onClick={() => {
+								const randomBanner =
+									availableBanners[
+										Math.floor(Math.random() * availableBanners.length)
+									]
+								setUserBanner(randomBanner)
+							}}
+						>
+							<div className='text-4xl font-bold text-white select-none'>?</div>
+						</div>
+					</div>
+					<Button
+						content='Valider'
+						onClick={() => setModalBannerIsOpen(false)}
+						backgroundColor='bg-orange'
+						additionalStyle='self-center w-[50%]'
+					/>
+				</div>
+			</Modal>
+			{/* Modal pour modifier les informations du compte */}
+			<Modal
+				isOpen={modalInfoIsOpen}
+				onClose={() => setModalInfoIsOpen(false)}
+				title='Modifier mon profil'
+			>
+				<div className='flex flex-col gap-5 min-w-[250px] sm:min-w-[350px] lg:min-w-[500px] min-h-[300px]'>
+					<div className='flex flex-col gap-5'>
+						<TextField label='Nom' />
+						<TextField label='Email' />
+					</div>
+					<Button
+						content='Valider'
+						backgroundColor='bg-orange'
+						additionalStyle='w-[50%] self-center'
+					/>
+				</div>
+			</Modal>
+			{/* Modal pour modifier le mot de passe */}
+			<Modal
+				isOpen={modalPasswordIsOpen}
+				onClose={() => setModalPasswordIsOpen(false)}
+				title='Modifier mon mot de passe'
+			>
+				<div className='flex flex-col gap-5 min-w-[250px] sm:min-w-[350px] lg:min-w-[500px] min-h-[300px]'>
+					<div className='flex flex-col gap-5'>
+						<TextField label='Mot de passe actuel' />
+						<TextField label='Nouveau mot de passe' />
+						<TextField label='Confirmer le nouveau mot de passe' />
+						<Button
+							content='Valider'
+							backgroundColor='bg-orange'
+							additionalStyle='w-[50%] self-center'
+						/>
+					</div>
+				</div>
+			</Modal>
+			{/* Modal pour supprimer le compte */}
+			<Modal
+				isOpen={modalDeleteAccountIsOpen}
+				onClose={() => {
+					setModalDeleteAccountIsOpen(false)
+					setDeleteAccountStateView('form')
+				}}
+				title='Supprimer mon compte'
+			>
+				<div className='flex flex-col gap-5 min-w-[250px] sm:min-w-[350px] lg:min-w-[500px] min-h-[200px]'>
+					<div className='flex flex-col gap-5'>
+						{deleteAccountStateView === 'form' ? (
+							<>
+								<TextField label='Mot de passe' />
+								<Button
+									content='Valider'
+									backgroundColor='bg-orange'
+									additionalStyle='w-[50%] self-center'
+									onClick={() => setDeleteAccountStateView('confirmation')}
+								/>
+							</>
+						) : (
+							<div className='flex flex-col h-[150px] justify-center gap-5 '>
+								<div className='text-center text-white sm:text-xl'>
+									Êtes-vous sûr de vouloir abandonner votre Yol ? Cette action
+									est irréversible.
+								</div>
+								<div className='flex gap-5 flex-wrap sm:flex-nowrap justify-center'>
+									<Button
+										content='Oui'
+										backgroundColor='bg-error'
+										textColor='text-white'
+										additionalStyle='w-[50%]'
+										onClick={() => {
+											setModalDeleteAccountIsOpen(false)
+											setDeleteAccountStateView('form')
+										}}
+									/>
+									<Button
+										content='Non'
+										backgroundColor='bg-orange'
+										additionalStyle='w-[50%]'
+										onClick={() => {
+											setModalDeleteAccountIsOpen(false)
+											setDeleteAccountStateView('form')
+										}}
+									/>
+								</div>
 							</div>
-							<p className='cursor-pointer'>Modifier mon mot de passe</p>
-						</div>
+						)}
 					</div>
-				</Box>
-				<Box additionalStyle='max-w-[300px] min-h-[400px] border-white border-2'>
-					<div className='flex flex-col items-center gap-5'>
-						<h2 className='text-center text-xl sm:text-4xl tracking-wider'>
-							Statistiques
-						</h2>
-						<div>
-							<ul className='sm:text-xl'>
-								<li>• 11 succès complétés</li>
-								<li>• 63 tâches quotidiennes achevées</li>
-								<li>• 1 évolution de Yol</li>
-								<li>• 2354 points d'expériences acquis</li>
-								<li>• Membre depuis le 02/01/23</li>
-							</ul>
-						</div>
-					</div>
-				</Box>
-			</div>
+				</div>
+			</Modal>
 		</div>
 	)
 }
---
+++ bestyol-front/src/app/components/profile/ProfileBanner.tsx
@@ -0,0 +1,74 @@
+import useWindowSize from '@/hooks/useWindowSize'
+import Image from 'next/image'
+
+interface ProfileBannerProps {
+	setModalAvatarIsOpen: (isOpen: boolean) => void
+	setModalBannerIsOpen: (isOpen: boolean) => void
+	avatar: string
+	banner: string
+}
+
+const ProfileBanner = ({
+	setModalAvatarIsOpen,
+	setModalBannerIsOpen,
+	avatar,
+	banner,
+}: ProfileBannerProps) => {
+	const windowSize = useWindowSize()
+	const username = 'Julie'
+	return (
+		<div className='relative flex items-center h-[200px] bg-lowOpacity'>
+			<div className='flex items-center'>
+				<div className='relative group'>
+					<Image
+						src={avatar}
+						height={200}
+						width={150}
+						alt='Image de profil'
+						className='ml-10 border-4 border-darkLowOpacity bg-purple'
+					/>
+					<Image
+						src='/assets/icons/clip-edit.svg'
+						height={30}
+						width={30}
+						alt='Icone de modification'
+						className='absolute bottom-0 right-0 cursor-pointer bg-white p-1 border-2 border-purple sm:hidden group-hover:block'
+						onClick={() => setModalAvatarIsOpen(true)}
+					/>
+				</div>
+				{windowSize.windowWidth > 500 && (
+					<div className='absolute top-2 left-40 border-collapse'>
+						<div className='text-2xl bg-white p-2 border-2 border-purple'>
+							Profil de{' '}
+							{username.length > 15 ? username.slice(0, 15) + '...' : username}
+						</div>
+						<div className='ml-2 w-6 overflow-hidden z-50 absolute bottom-[-73%]'>
+							<div className='h-10 bg-white rotate-45 transform origin-top-right border-2 border-purple' />
+						</div>
+					</div>
+				)}
+			</div>
+			{windowSize.windowWidth > 500 && (
+				<>
+					<Image
+						src={banner}
+						height={100}
+						width={300}
+						alt='Fond du profil'
+						className='absolute right-0 bottom-0'
+					/>
+					<Image
+						src='/assets/icons/clip-edit.svg'
+						height={30}
+						width={30}
+						alt='Icone de modification'
+						className='absolute top-1 right-1 cursor-pointer bg-white p-1 border-1 border-purple'
+						onClick={() => setModalBannerIsOpen(true)}
+					/>
+				</>
+			)}
+		</div>
+	)
+}
+
+export default ProfileBanner
