105917a82f322df44e7e142a122cc699fbf1cd2e
Vincent Flebus <vflebus@gmail.com>
2023-06-01 15:52:22 +0200

    fetch user success order by progress

--- src/app/(pages)/(connected-user)/achievements/page.tsx
+++ src/app/(pages)/(connected-user)/achievements/page.tsx
@@ -7,6 +7,7 @@ import {
 	useFetchAllSuccessQuery,
 	useFetchAllUserSuccessQuery,
 } from '@/services/queries/success'
+import { UserSuccess } from '@/type/success.type'
 
 interface AchievementProps {
 	title: string
@@ -17,7 +18,8 @@ interface AchievementProps {
 }
 
 const AchievementsPage = () => {
-	const { data: userSuccess } = useFetchAllUserSuccessQuery()
+	const { data: userSuccess, isLoading: userSuccessLoading } =
+		useFetchAllUserSuccessQuery()
 	const { data: success } = useFetchAllSuccessQuery()
 	achievementsList.sort((a, b) => {
 		if (a.progress / a.goal > b.progress / b.goal) return -1
@@ -30,24 +32,26 @@ const AchievementsPage = () => {
 	})
 
 	return (
-		<>
-			<h1 className='text-4xl text-white my-[2rem]'>MES SUCCÈS</h1>
-			<div className='w-full flex flex-col items-center'>
-				<Box centerItems additionalStyle='gap-y-[2rem] lg:w-[65%] mb-[2rem]'>
-					{achievementsList.map((achievement: AchievementProps) => {
-						return (
-							<AchievementTile
-								title={achievement.title}
-								description={achievement.description}
-								xp={achievement.xp}
-								goal={achievement.goal}
-								progress={achievement.progress}
-							/>
-						)
-					})}
-				</Box>
-			</div>
-		</>
+		userSuccess && (
+			<>
+				<h1 className='text-4xl text-white my-[2rem]'>MES SUCCÈS</h1>
+				<div className='w-full flex flex-col items-center'>
+					<Box centerItems additionalStyle='gap-y-[2rem] lg:w-[65%] mb-[2rem]'>
+						{userSuccess.map((achievement: UserSuccess) => {
+							return (
+								<AchievementTile
+									title={achievement.success.title}
+									description={achievement.success.description}
+									xp={achievement.success.success_xp}
+									goal={achievement.success.amount_needed}
+									progress={achievement.actual_amount}
+								/>
+							)
+						})}
+					</Box>
+				</div>
+			</>
+		)
 	)
 }
 
