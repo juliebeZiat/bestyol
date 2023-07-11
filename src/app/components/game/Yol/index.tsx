65714546f659fe7432306d26fb2b23bea871d44b
Julie Beziat <juliebeziat.m@gmail.com>
2023-07-11 15:46:33 +0200

    fix: resolve syntax error

--- src/app/components/game/Yol/index.tsx
+++ src/app/components/game/Yol/index.tsx
@@ -36,7 +36,6 @@ const YolBox = () => {
 		)
 	}
 
-
 	if (isYolEvolving(yol.data)) {
 		dispatch(
 			setNotification({
@@ -45,54 +44,54 @@ const YolBox = () => {
 			}),
 		)
 
-	const getYolHeight = () => {
-		if (yol.data.species.stage === SpeciesStages.ADO) return 'h-[70%]'
-		if (yol.data.species.stage === SpeciesStages.FINAL) return 'h-full'
-		return 'h-[50%]'
-	}
+		const getYolHeight = () => {
+			if (yol.data.species.stage === SpeciesStages.ADO) return 'h-[70%]'
+			if (yol.data.species.stage === SpeciesStages.FINAL) return 'h-full'
+			return 'h-[50%]'
+		}
 
-	const animation =
-		getEvolutionStep(yol.data) === 0
-			? 'animate-wiggleInfinite'
-			: 'animate-jumpInfinite'
+		const animation =
+			getEvolutionStep(yol.data) === 0
+				? 'animate-wiggleInfinite'
+				: 'animate-jumpInfinite'
 
-	return (
-		<div
-			className={`h-full w-full flex flex-col justify-center  ${
-				isYolEvolving(yol.data) && 'cursor-pointer'
-			}`}
-		>
-			<div className='h-full w-full flex gap-x-8 lg:gap-x-16 justify-center flex-wrap lg:flex-nowrap items-center'>
-				<div
-					className={`p-4 pixel-corners-items ${theme.pixelBorderColor} ${theme.secondaryBackgroundColor} ${theme.borderColor} text-white`}
-				>
-					<h2 className='uppercase text-lg mb-2'>Informations</h2>
-					<p>Nom: ✨ {yol.data.name} ✨</p>
-					<p>Né le: {getFormattedDate(yol.data.createdAt)}</p>
-					<p>Espèce: {yol.data.species.name}</p>
-					<p>Stade d'évolution: {yol.data.species.stage}</p>
-				</div>
-				<div
-					onClick={handleEvolveYol}
-					className='h-full flex items-center justify-center'
-				>
-					<Image
-						src={
-							yol.data.species.stage === SpeciesStages.EGG
-								? yol.data.species.image
-								: yol.data.species.gif
-						}
-						width={150}
-						height={150}
-						alt='yol'
-						className={`${getYolHeight()} w-auto ${
-							isYolEvolving(yol.data) ? `${animation} origin-bottom` : ''
-						}`}
-					/>
+		return (
+			<div
+				className={`h-full w-full flex flex-col justify-center  ${
+					isYolEvolving(yol.data) && 'cursor-pointer'
+				}`}
+			>
+				<div className='h-full w-full flex gap-x-8 lg:gap-x-16 justify-center flex-wrap lg:flex-nowrap items-center'>
+					<div
+						className={`p-4 pixel-corners-items ${theme.pixelBorderColor} ${theme.secondaryBackgroundColor} ${theme.borderColor} text-white`}
+					>
+						<h2 className='uppercase text-lg mb-2'>Informations</h2>
+						<p>Nom: ✨ {yol.data.name} ✨</p>
+						<p>Né le: {getFormattedDate(yol.data.createdAt)}</p>
+						<p>Espèce: {yol.data.species.name}</p>
+						<p>Stade d'évolution: {yol.data.species.stage}</p>
+					</div>
+					<div
+						onClick={handleEvolveYol}
+						className='h-full flex items-center justify-center'
+					>
+						<Image
+							src={
+								yol.data.species.stage === SpeciesStages.EGG
+									? yol.data.species.image
+									: yol.data.species.gif
+							}
+							width={150}
+							height={150}
+							alt='yol'
+							className={`${getYolHeight()} w-auto ${
+								isYolEvolving(yol.data) ? `${animation} origin-bottom` : ''
+							}`}
+						/>
+					</div>
 				</div>
 			</div>
-		</div>
-	)
+		)
+	}
 }
-
 export default YolBox
