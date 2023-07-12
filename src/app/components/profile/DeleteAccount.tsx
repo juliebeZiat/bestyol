59f3349de9ef384c038257eab815468a656ca88b
Julie Beziat <juliebeziat.m@gmail.com>
2023-07-12 19:40:38 +0200

    fix: clean useless code

--- src/app/components/profile/DeleteAccount.tsx
+++ src/app/components/profile/DeleteAccount.tsx
@@ -12,7 +12,7 @@ interface DeleteAccountProps {
 
 const DeleteAccount = ({ closeModal }: DeleteAccountProps) => {
 	const dispatch = useAppDispatch()
-	const { user, theme } = useAppSelector((state: RootState) => state.user)
+	const { user } = useAppSelector((state: RootState) => state.user)
 	const [requestError, setRequestError] = useState<string | null>()
 	const [values, setValues] = useState({
 		password: '',
@@ -22,7 +22,6 @@ const DeleteAccount = ({ closeModal }: DeleteAccountProps) => {
 
 	const handleDelete = async () => {
 		const data = { userId: user.id, password: values.password }
-		console.log(data)
 		await mutateAsync(data, {
 			onSuccess: async () => {
 				dispatch(logout())
@@ -35,64 +34,6 @@ const DeleteAccount = ({ closeModal }: DeleteAccountProps) => {
 
 	return (
 		<div className='flex flex-col gap-5 min-w-[250px] sm:min-w-[350px] lg:min-w-[500px] min-h-[200px]'>
-			{/* <div className='flex flex-col gap-5'>
-				{deleteAccountStateView === 'form' ? (
-					<>
-						<TextField
-							label='Mot de passe'
-							inputFocus
-							inputType='password'
-							value={values.password}
-							onChange={(e) =>
-								setValues({
-									...values,
-									password: e.target.value,
-								})
-							}
-						/>
-						<Button
-							content='Valider'
-							backgroundColor={theme.secondaryBackgroundColor}
-							textColor='text-white'
-							additionalStyle='w-[50%] self-center'
-							onClick={() => setDeleteAccountStateView('confirmation')}
-						/>
-					</>
-				) : (
-					<div className='flex flex-col h-[150px] justify-center gap-5 '>
-						<div className='text-center text-white sm:text-xl'>
-							Êtes-vous sûr de vouloir abandonner votre Yol ? Cette action est
-							irréversible.
-						</div>
-						<div className='flex gap-5 flex-wrap sm:flex-nowrap justify-center'>
-							<Button
-								content='Oui'
-								backgroundColor='bg-error'
-								textColor='text-white'
-								additionalStyle='w-[50%]'
-								onClick={handleDelete}
-							/>
-							<Button
-								content='Non'
-								backgroundColor={theme.secondaryBackgroundColor}
-								textColor='text-white'
-								additionalStyle='w-[50%]'
-								onClick={() => {
-									closeModal()
-									setDeleteAccountStateView('form')
-								}}
-							/>
-						</div>
-						{isError && (
-							<div>
-								<p className='text-lg text-error'>
-									{requestError || 'Mot de passe incorrect'}
-								</p>
-							</div>
-						)}
-					</div>
-				)}
-			</div> */}
 			<div>
 				<h2 className='text-center text-white sm:text-xl'>
 					Pour supprimer votre compte, veuillez entrer votre mot de passe
